0dd706cbe3ca16db50a86d08a9a0f8eaa6a7dbeb
arash <arashveer01@gmail.com>
2023-08-30 18:12:58 -0700

    metadata use

--- src/App.js
+++ src/App.js
@@ -3,111 +3,124 @@ import React, { useRef } from "react";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = React.useState([]);
-  const [subreddit, setSubreddit] = React.useState("news");
-  const api_url =
-    "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=10";
+    const [data, setData] = React.useState([]);
+    const [subreddit, setSubreddit] = React.useState("news");
 
-  let inputRef = React.useRef();
-  let btnRef = useRef();
+    let inputRef = React.useRef();
+    let btnRef = useRef();
 
-  React.useEffect(() => {
-    axios
-      .get(api_url)
-      .then((response) => {
-        setData(response.data.data.children);
-        btnRef.current.removeAttribute("disabled");
-        btnRef.current.classList.remove("btn--loading");
-      })
-      .catch((err) => {
-        console.log(err);
-        btnRef.current.removeAttribute("disabled");
-        btnRef.current.classList.remove("btn--loading");
-      });
-  }, [subreddit]);
+    React.useEffect(() => {
+        const api_url =
+            "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=15";
+        axios
+            .get(api_url)
+            .then((response) => {
+                setData(response.data.data.children);
+                btnRef.current.removeAttribute("disabled");
+                btnRef.current.classList.remove("btn--loading");
+            })
+            .catch((err) => {
+                console.log(err);
+                btnRef.current.removeAttribute("disabled");
+                btnRef.current.classList.remove("btn--loading");
+            });
+    }, [subreddit]);
 
-  function changeSub() {
-    if (subreddit == inputRef.current.value) {
-      alert("You are already on this subreddit.");
-      return;
+    function changeSub() {
+        if (subreddit == inputRef.current.value) {
+            alert("You are already on this subreddit.");
+            return;
+        }
+        // setSubreddit to change the subreddit which calls useEffect to get data for this new subreddit
+        btnRef.current.setAttribute("disabled", "disabled");
+        btnRef.current.classList.add("btn--loading");
+        setSubreddit(inputRef.current.value);
     }
-    // setSubreddit to change the subreddit which calls useEffect to get data for this new subreddit
-    btnRef.current.setAttribute("disabled", "disabled");
-    btnRef.current.classList.add("btn--loading");
-    setSubreddit(inputRef.current.value);
-  }
 
-  return (
-    <div className="App container">
-      <h1>Reddit News </h1>
-      <div className="sub">
-        <ul className="tags">
-          <li>
-            <a
-              onClick={() => {
-                setSubreddit("worldnews");
-              }}
-            >
-              World
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                setSubreddit("canada");
-              }}
-            >
-              Canada
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                setSubreddit("sports");
-              }}
-            >
-              Sports
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                setSubreddit("technology");
-              }}
-            >
-              Technology
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                setSubreddit("politics");
-              }}
-            >
-              Politics
-            </a>
-          </li>
-        </ul>
-        <input
-          className="inputField"
-          type="text"
-          placeholder="type subreddit"
-          name="subReddit"
-          ref={inputRef}
-        />
-        <button className="btn" ref={btnRef} onClick={changeSub}>
-          Change
-        </button>
-      </div>
-      <div className="cards">
-        {data.map((item, index) => (
-          <div key={index}>
-            <News data={item.data} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="App container">
+            <h1>Reddit News </h1>
+            <div className="sub">
+                <ul className="tags">
+                    <li>
+                        <a
+                            onClick={() => {
+                                setSubreddit("worldnews");
+                            }}
+                        >
+                            World
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            onClick={() => {
+                                setSubreddit("canada");
+                            }}
+                        >
+                            Canada
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            onClick={() => {
+                                setSubreddit("sports");
+                            }}
+                        >
+                            Sports
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            onClick={() => {
+                                setSubreddit("technology");
+                            }}
+                        >
+                            Technology
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            onClick={() => {
+                                setSubreddit("politics");
+                            }}
+                        >
+                            Politics
+                        </a>
+                    </li>
+                </ul>
+                <input
+                    className="inputField"
+                    type="text"
+                    placeholder="type subreddit"
+                    name="subReddit"
+                    ref={inputRef}
+                />
+                <button className="btn" ref={btnRef} onClick={changeSub}>
+                    Change
+                </button>
+            </div>
+            <div className="cards">
+                {data.map((item, index) =>
+                    !item.data.stickied ? (
+                        <div key={item.data.url}>
+                            <News data={item.data} />
+                            {/* {console.log(item)} */}
+                        </div>
+                    ) : (
+                        ""
+                    )
+                )}
+            </div>
+            <div className="footer_text">
+                <p>
+                    Made by{" "}
+                    <a href="https://arashveer.com" target="_blank">
+                        Arashveer Garaya
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/News.js
+++ src/components/News.js
@@ -1,22 +1,86 @@
 import PropTypes from "prop-types";
+import axios from "axios";
+import React, { useRef } from "react";
 
-const News = ({ data }) => {
-  return (
-    <div className="news">
-      <h3>
-        <a href={data.url}>{data.title}</a>
-      </h3>
-      <p className="des">From: {data.domain}</p>
-    </div>
-  );
+const News = (props) => {
+    const [metaData, setmetaData] = React.useState([]);
+    function timeSince(epoch) {
+        var date = parseInt(String(epoch));
+        date = date * 1000;
+        date = Date.now() - date;
+        var seconds = date / 1000;
+
+        var interval = seconds / 31536000;
+
+        if (interval > 1) {
+            return Math.floor(interval) + " years ago";
+        }
+        interval = seconds / 2592000;
+        if (interval > 1) {
+            return Math.floor(interval) + " months ago";
+        }
+        interval = seconds / 86400;
+        if (interval > 1) {
+            return Math.floor(interval) + " days ago";
+        }
+        interval = seconds / 3600;
+        if (interval > 1) {
+            return Math.floor(interval) + " hours ago ";
+        }
+        interval = seconds / 60;
+        if (interval > 1) {
+            return Math.floor(interval) + " mins ago";
+        }
+        return Math.floor(seconds) + " secs ago";
+    }
+    if (metaData.length < 1) {
+        axios
+            .get(`https://jsonlink.io/api/extract?url=${props.data.url}`)
+            .then((response) => {
+                setmetaData(response.data);
+                // console.log(metaData.title);
+            })
+            .catch((err) => {
+                console.log("Hello", err);
+            });
+    }
+
+    return (
+        <div className="news">
+            {metaData.images && metaData.images[0] ? (
+                <div className="news_picture">
+                    <img src={metaData.images[0]} alt="" />
+                </div>
+            ) : (
+                ""
+            )}
+
+            <div className="news_content">
+                <p className="news_time">{timeSince(props.data.created_utc)}</p>
+                <h3>
+                    <a href={props.data.url} target="_blank">
+                        {props.data.title}
+                    </a>
+                </h3>
+                {metaData.description ? (
+                    <p className="news_desc">{metaData.description}</p>
+                ) : (
+                    ""
+                )}
+                <p className="domain">From: {props.data.domain}</p>
+            </div>
+        </div>
+    );
 };
 
 News.defaultProps = {
-  title: "News Article",
+    data: [],
+    subReddit: "",
 };
 
 News.propTypes = {
-  title: PropTypes.string,
+    data: PropTypes.any,
+    subReddit: PropTypes.any,
 };
 
 export default News;
